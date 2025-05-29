d21ae8554cbb452429be54f380a86768c87da0e2
Nathan-Sasa <Jonathanbikuta05@gmail.com>
2025-05-29 23:30:38 +0100

    ajout du formulaier d'ajout d'un produit

--- src/app/shared/components/edit-product/edit-product.component.ts
+++ src/app/shared/components/edit-product/edit-product.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Exr1Component } from '../../../component/exr1/exr1.component';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-product',
-  imports: [],
+  imports: [ CommonModule],
   standalone: true,
   templateUrl: './edit-product.component.html',
   styleUrl: './edit-product.component.css'
